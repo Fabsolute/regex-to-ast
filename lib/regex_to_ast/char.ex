18aa1a706c6017b695d5b201bb70b89e87d1c75f
Ahmet Turk <ahtu@boozt.com>
2023-10-09 20:17:44 +0200

    -

--- lib/regex_to_ast/char.ex
+++ lib/regex_to_ast/char.ex
@@ -13,43 +13,86 @@ defmodule RegexToAST.Char do
     ?d => ?\d
   }
 
-  @special_char_keys Map.keys(@special_char_map)
-
   hex_char = ascii_char([?0..?9, ?a..?f, ?A..?F])
   octal_char = ascii_char([?0..?7])
-  special_char = ascii_char(@special_char_keys)
 
-  defcombinator :capture_hex,
-                ignore(string("\\"))
-                |> ignore(string("x"))
-                |> choice([
-                  hex_char
-                  |> concat(hex_char),
-                  ignore(string("{"))
-                  |> concat(hex_char)
-                  |> repeat(hex_char)
-                  |> ignore(string("}"))
-                ])
-                |> reduce({List, :to_integer, [16]})
+  defcombinatorp :capture_hex,
+                 ignore(ascii_char([?\\]))
+                 |> ignore(ascii_char([?x]))
+                 |> choice([
+                   hex_char
+                   |> concat(hex_char),
+                   ignore(ascii_char([?{]))
+                   |> times(hex_char, min: 1, max: 4)
+                   |> ignore(ascii_char([?}]))
+                 ])
+                 |> reduce({List, :to_integer, [16]})
 
-  defcombinator :capture_octal,
-                ignore(string("\\"))
-                |> concat(octal_char)
-                |> concat(octal_char)
-                |> concat(octal_char)
-                |> reduce({List, :to_integer, [8]})
+  defcombinatorp :capture_octal,
+                 ignore(ascii_char([?\\]))
+                 |> concat(octal_char)
+                 |> concat(octal_char)
+                 |> concat(octal_char)
+                 |> reduce({List, :to_integer, [8]})
 
-  defcombinator :capture_special,
-                ignore(string("\\"))
-                |> concat(special_char)
-                |> reduce({:escape_special_char, []})
+  defcombinatorp :capture_special,
+                 ignore(ascii_char([?\\]))
+                 |> utf8_char([])
+                 |> reduce(:escape_special_char)
 
-  defparsec :parse_char,
-            choice([
-              parsec(:capture_hex),
-              parsec(:capture_octal),
-              parsec(:capture_special)
-            ])
+  defcombinatorp :capture_char,
+                 choice([
+                   parsec(:capture_hex),
+                   parsec(:capture_octal),
+                   parsec(:capture_special),
+                   utf8_char(
+                     not: ?^,
+                     not: ?.,
+                     not: ?[,
+                     not: ?$,
+                     not: ?(,
+                     not: ?),
+                     not: ?|,
+                     not: ?*,
+                     not: ?+,
+                     not: ??,
+                     not: ?\\,
+                     not: ?]
+                   )
+                 ])
 
-  defp escape_special_char([char]), do: Map.get(@special_char_map, char)
+  defcombinatorp :parse_range,
+                 parsec(:capture_char)
+                 |> ignore(ascii_char([?-]))
+                 |> parsec(:capture_char)
+                 |> post_traverse(:wrap_range)
+
+  defcombinatorp :parse_char_class,
+                 ignore(ascii_char([?[]))
+                 |> optional(ascii_char([?^]))
+                 |> tag(:reverse)
+                 |> times(
+                   choice([
+                     parsec(:parse_range),
+                     parsec(:capture_char)
+                   ]),
+                   min: 1
+                 )
+                 |> ignore(ascii_char([?]]))
+                 |> reduce(:tag_char_class)
+
+  defparsec :parse, parsec(:parse_char_class) |> eos()
+
+  defp escape_special_char([char]), do: Map.get(@special_char_map, char, char)
+
+  defp wrap_range(_rest, [char2, char1], context, _line, _offset) do
+    if char2 >= char1 do
+      {[{:range, char1, char2}], context}
+    else
+      {:error, "Not in range #{[char1]} and #{[char2]}"}
+    end
+  end
+
+  defp tag_char_class([{:reverse, [?^]} | rest]), do: {:comp_class, rest}
+  defp tag_char_class([{:reverse, []} | rest]), do: {:char_class, rest}
 end
